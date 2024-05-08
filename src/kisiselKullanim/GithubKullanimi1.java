ffc73cd90a70e180aed37562da6f446e9044fbd3
Serdar1048 <serdardedebas1@gmail.com>
2024-05-08 17:29:23 +0300

    main branch degişiklik

--- src/kisiselKullanim/GithubKullanimi1.java
+++ src/kisiselKullanim/GithubKullanimi1.java
@@ -5,5 +5,8 @@ public class GithubKullanimi1 {
         System.out.println("Hello world");
 
         System.out.println("That's it");
+
+        System.out.println("main branch değişşkilik 1.satır");
+        System.out.println("main branch değişşkilik 2.satır");
     }
 }
